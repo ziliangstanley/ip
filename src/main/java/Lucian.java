a90145a3c7aeb7435994a67573b95e0c0b3a27b0
Stanley Beh <ziliangstanley@gmail.com>
2025-01-23 23:13:44 +0800

    Add echo functionality

--- src/main/java/Lucian.java
+++ src/main/java/Lucian.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lucian {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,7 +11,21 @@ public class Lucian {
         System.out.println("Hey! I'm Lucian\n" + "How can I help?");
         System.out.println("____________________________________________________________");
 
-        System.out.println("Bye. See you around.");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. See you around.");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
     }
 }
