0f7c31f5386aa47e27178187ecad83fa0f12f750
Stanley Beh <ziliangstanley@gmail.com>
2025-01-23 23:42:04 +0800

    Add list functionality

--- src/main/java/Lucian.java
+++ src/main/java/Lucian.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Lucian {
     public static void main(String[] args) {
@@ -7,11 +8,14 @@ public class Lucian {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> listOfInputs = new ArrayList<String>();
+
         System.out.println("____________________________________________________________");
         System.out.println("Hey! I'm Lucian\n" + "How can I help?");
         System.out.println("____________________________________________________________");
 
-        Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNext()) {
             String userInput = scanner.nextLine();
@@ -20,9 +24,16 @@ public class Lucian {
                 System.out.println("Bye. See you around.");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < listOfInputs.size(); i++) {
+                    System.out.println(i+1 + ". " + listOfInputs.get(i));
+                }
+                System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                listOfInputs.add(userInput);
                 System.out.println("____________________________________________________________");
             }
         }
