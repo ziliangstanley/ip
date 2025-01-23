226f1ab07fe8e61993c0f6b58a2e09398870e701
Stanley Beh <ziliangstanley@gmail.com>
2025-01-24 01:50:50 +0800

    Create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Alright, I've marked this task as done:");
+        System.out.println(this.toString());
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+        System.out.println("Alright, I've marked this task as not done yet:");
+        System.out.println(this.toString());
+    }
+}
