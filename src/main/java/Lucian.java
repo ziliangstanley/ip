f1cc170098a4e9ea0cea2bcfaadf3b61d905a853
Stanley Beh <ziliangstanley@gmail.com>
2025-01-24 01:51:35 +0800

    Add mark as done functionality

--- src/main/java/Lucian.java
+++ src/main/java/Lucian.java
@@ -10,7 +10,7 @@ public class Lucian {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> listOfInputs = new ArrayList<String>();
+        ArrayList<Task> listOfTasks = new ArrayList<Task>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hey! I'm Lucian\n" + "How can I help?");
@@ -19,23 +19,31 @@ public class Lucian {
 
         while (scanner.hasNext()) {
             String userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
             if (userInput.equals("bye")) {
-                System.out.println("____________________________________________________________");
                 System.out.println("Bye. See you around.");
-                System.out.println("____________________________________________________________");
                 break;
             } else if (userInput.equals("list")) {
-                System.out.println("____________________________________________________________");
-                for (int i = 0; i < listOfInputs.size(); i++) {
-                    System.out.println(i+1 + ". " + listOfInputs.get(i));
+                for (int i = 0; i < listOfTasks.size(); i++) {
+                    Task currentTask = listOfTasks.get(i);
+                    System.out.println(i + 1 + "." + currentTask.toString());
                 }
-                System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("mark")) {
+                String index = userInput.split(" ")[1];
+                int idx = Integer.parseInt(index);
+                Task currentTask = listOfTasks.get(idx - 1);
+                currentTask.markAsDone();
+            } else if (userInput.startsWith("unmark")) {
+                String index = userInput.split(" ")[1];
+                int idx = Integer.parseInt(index);
+                Task currentTask = listOfTasks.get(idx - 1);
+                currentTask.markAsNotDone();
             } else {
-                System.out.println("____________________________________________________________");
                 System.out.println("added: " + userInput);
-                listOfInputs.add(userInput);
-                System.out.println("____________________________________________________________");
+                Task newTask = new Task(userInput);
+                listOfTasks.add(newTask);
             }
+            System.out.println("____________________________________________________________");
         }
 
     }
