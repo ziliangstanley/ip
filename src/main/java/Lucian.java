dcaf048897240e3b31af294ecd12521b6adf6011
Stanley Beh <ziliangstanley@gmail.com>
2025-01-23 22:43:02 +0800

    Change Duke.java to Lucian.java and add greetings dialogue

---
+++ src/main/java/Lucian.java
@@ -0,0 +1,15 @@
+public class Lucian {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hey! I'm Lucian\n" + "How can I help?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Bye. See you around.");
+        System.out.println("____________________________________________________________");
+    }
+}
