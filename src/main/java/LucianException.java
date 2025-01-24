dca60a6ae781acec546469eb74c2733d66ff569e
Stanley Beh <ziliangstanley@gmail.com>
2025-01-24 14:21:50 +0800

    Create LucianException class

---
+++ src/main/java/LucianException.java
@@ -0,0 +1,10 @@
+public class LucianException extends Exception{
+    public LucianException(String string) {
+        super(string);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
