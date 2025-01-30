ddc1ea70e8623b7c1f1b84298fe69a36934936fb
Stanley Beh <ziliangstanley@gmail.com>
2025-01-30 22:15:04 +0800

    Add save function to files

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,11 @@ public class Event extends Task {
     }
 
     @Override
+    public String toFileFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + this.from + " | " + this.to;
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
     }
